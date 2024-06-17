d92abfd752a4570970728e04dd5d6ab140a3c0f2
Gabriel programador web <147432201+Gabrielwebdevelop@users.noreply.github.com>
2024-06-17 08:53:50 -0300

    Desafio - DOM JS 5, calcular media velocidade
    Utilizando HTML CSS e JavaScript, a velocidade média de uma via é 60km/h, se a velocidade for igual a 60, mostre em verde prossiga, se for igual a 80 mostre em amarelo reduza velocidade, se for maior que 80 mostre em vermelho, Você foi multado.

---
+++ Desafio - DOM JS 5/script.js
@@ -0,0 +1,31 @@
+var corpo = document.body;
+corpo.style.backgroundColor = 'black';
+corpo.style.color = 'white';
+function velocidadeAtual(){
+    var velocidade = document.getElementById('Nvelocidade').value;
+    var velocidadeMedia = 60;
+    var mostrarResultado = document.getElementById('mostrarResultado');
+    if(velocidade == velocidadeMedia){
+        mostrarResultado.innerHTML = `A sua velocidade média é: ${ velocidade}`;
+        mostrarResultado.style.color = 'green';
+       
+        mostrarResultado.style.width = '200px';
+        mostrarResultado.style.fontSize = '20px';
+    }else if(velocidade == 80){
+        mostrarResultado.innerHTML = `Sua velocidade está: ${velocidade}, reduzias ela `;
+        mostrarResultado.style.color = 'yellow';
+        mostrarResultado.style.fontSize = '20px';
+        mostrarResultado.style.width = '200px';
+    }else if(velocidade > 80){
+        mostrarResultado.innerHTML = `Reduza velocidade está ${velocidade} você foi multado!`;
+        mostrarResultado.style.color = 'red';
+        mostrarResultado.style.fontSize = '20px';
+        mostrarResultado.style.width = '200px';
+    }else{
+        mostrarResultado.innerHTML = `Você está abaixo de 60: ${velocidade}`;
+        mostrarResultado.style.color = 'blue';
+        mostrarResultado.style.fontSize = '20px';
+        mostrarResultado.style.width = '200px';
+        
+    }
+}
